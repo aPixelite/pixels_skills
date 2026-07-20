fe2ecf1fcda5912ab872f75cd4e6ea3f3a30c3c4
apixelite <apixelxtra@gmail.com>
2026-07-20 22:32:20 +0200

    removed Icons list

--- src/main/java/net/apixelite/skills/skills/icons/Icons.java
+++ src/main/java/net/apixelite/skills/skills/icons/Icons.java
@@ -2,19 +2,13 @@ package net.apixelite.skills.skills.icons;
 
 import net.apixelite.skills.PixelsSkills;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Icons {
-
-    public static final List<ItemStack> ICON_ITEMS = new ArrayList<>();
 
     public static final Item COMBAT_SKILL_ICON = registerIconItem("combat");
     public static final Item MINING_SKILL_ICON = registerIconItem("mining");
